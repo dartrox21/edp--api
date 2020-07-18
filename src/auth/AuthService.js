047b82438d4d90d9f27c49e6d2051428166f694c
dartrox21 <pakrisparkour@gmailcom>
2020-07-17 19:09:45 -0500

    solving problems

--- src/auth/AuthService.js
+++ src/auth/AuthService.js
@@ -10,20 +10,21 @@ class AuthService {
         this.login = this.login.bind(this);
      }
 
-    async login(req, res) {
+    async login(req, res, next) {
         const userLogin = req.body;
-        const user = await UserService.findByEmail(req.body.email);
+        let user = await UserService.findByEmail(req.body.email);
         const hasValidCredentials = await bcrypt.compare(userLogin.password, user.password);
         if(!hasValidCredentials) {
             throw CustomValidateException.conflict().errorMessage(CustomErrorMessages.BAD_CREDENTIALS).build();
         }
+        user = user.toObject();
         delete user.password;
-        jwt.sign(user.toJSON(), process.env.SECRET, {expiresIn: process.env.EXP_DATE}, (err, token) => {
+        jwt.sign(user, process.env.SECRET, {expiresIn: process.env.EXP_DATE}, (err, token) => {
             if(err) {
-                throw CustomValidateException.conflict().errorMessage(CustomErrorMessages.BAD_CREDENTIALS).build();
+                next(CustomValidateException.conflict().errorMessage(CustomErrorMessages.BAD_CREDENTIALS).build());
             } else {
                 res.setHeader('Authorization', `Bearer ${token}`);
-                res.status(HttpStatus.OK).json(token)
+                res.status(HttpStatus.OK).json({user, token});
             }
         });
     }
