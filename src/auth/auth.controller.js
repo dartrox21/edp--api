a466724ac41efe944c6d35ae97c080d648a82983
Saúl Ruiz <>
2021-05-01 14:23:59 +0000

    validate token

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const { validateAuthUser } = require('./auth.middleware');
 const AuthService = require('./auth.service');
 const { asyncWrapper } = require('../utils/util.functions');
+const HttpStatus = require('http-status-codes');
 
 router.post('/auth/login', validateAuthUser, asyncWrapper(AuthService.login));
 
 router.post('/auth/logout', asyncWrapper(AuthService.logout));
+
+router.post('/auth/validate-token', (req, res) => res.status(HttpStatus.OK).send())
 
 module.exports = router;
